e6ac9e879dc8d0f504350cfef2eb035b7d6b2511
Cao Quỳnh Anh <quynhanhcao99@gmail.com>
2020-04-25 15:25:59 +0700

    V 1.0

---
+++ app/src/main/java/vn/edu/ntu/quynhanh/caoquynhanh_59130033_simplewidget/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.quynhanh.caoquynhanh_59130033_simplewidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
